ab7491382cc4fec879c0672698c2ba7fcf20570f
seongje00416 <seongje00416@gmail.com>
2025-11-07 17:44:16 +0900

    feat: Swagger Setting and Create Test API

--- api/src/main/java/fooding/im/api/config/SwaggerConfig.java
+++ api/src/main/java/fooding/im/api/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package fooding.im.api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,6 +14,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Server")
+                ))
                 .info(new Info()
                         .title("Fooding Webflux API Server")
                         .description("Fooding WebFlux API Documentation")
