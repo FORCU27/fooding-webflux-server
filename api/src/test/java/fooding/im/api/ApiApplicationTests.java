bd41b68f938028282611d11de5f544a5b58ab497
seongje00416 <seongje00416@gmail.com>
2025-11-07 17:03:17 +0900

    Project Initialize

---
+++ api/src/test/java/fooding/im/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fooding.im.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
