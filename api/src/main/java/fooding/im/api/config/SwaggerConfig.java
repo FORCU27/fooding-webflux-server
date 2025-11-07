41cbbd882b5c402be360a801aba4035399bb4478
seongje00416 <seongje00416@gmail.com>
2025-11-07 17:37:20 +0900

    멀티 모듈 적용

---
+++ api/src/main/java/fooding/im/api/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package fooding.im.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fooding Webflux API Server")
+                        .description("Fooding WebFlux API Documentation")
+                        .version("v1.0.0"));
+    }
+}
---
+++ api/src/main/java/fooding/im/api/controller/TestController.java
@@ -0,0 +1,8 @@
+package fooding.im.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v3/test")
+public class TestController {
+
+}
