4a360a539e4d7a935114d7a1263f42d138a027a6
seongje00416 <seongje00416@gmail.com>
2025-11-24 17:49:57 +0900

    조회 기능만 사용

--- api/src/main/java/fooding/im/api/controller/pos/reward/PosRewardV3Controller.java
+++ api/src/main/java/fooding/im/api/controller/pos/reward/PosRewardV3Controller.java
@@ -1,7 +1,6 @@
 package fooding.im.api.controller.pos.reward;
 
 import fooding.im.api.dto.request.pos.reward.GetPosRewardRequest;
-import fooding.im.api.dto.request.pos.reward.UpdateRewardLogRequest;
 import fooding.im.api.dto.response.pos.reward.GetPosRewardResponse;
 import fooding.im.api.service.pos.reward.PosRewardService;
 import fooding.im.core.common.ApiResult;
@@ -10,13 +9,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -34,30 +29,4 @@ public class PosRewardV3Controller {
         return service.list( request ).map( ApiResult::ok );
     }
 
-    @PatchMapping("/{id}/cancel")
-    @Operation( summary = "특정 리워드 로그에 대한 적립 취소" )
-    public Mono<ApiResult<Void>> cancelReward(
-            @PathVariable Long id,
-            @RequestBody UpdateRewardLogRequest request
-    ){
-        return service.cancel( id, request ).then( Mono.fromSupplier(ApiResult::ok));
-    }
-
-    @PatchMapping( "/{id}/approve" )
-    @Operation( summary = "특정 리워드 로그에 대한 적립 허용" )
-    public Mono<ApiResult<Void>> approveReward(
-            @PathVariable Long id,
-            @RequestBody UpdateRewardLogRequest request
-    ){
-        return service.approve( id, request ).then( Mono.fromSupplier(ApiResult::ok) );
-    }
-
-    @PatchMapping( "/{id}/memo" )
-    @Operation( summary = "특정 리워드 로그에 메모 변경" )
-    public Mono<ApiResult<Void>> updateMemo(
-            @PathVariable Long id,
-            @RequestBody String memo
-    ){
-        return service.updateMemo( id, memo ).then( Mono.fromSupplier(ApiResult::ok) );
-    }
 }
